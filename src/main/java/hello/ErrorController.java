4ac2a2d35abfb43892b256e2e7a61dce52884dbc
Marcos Carvalho <marcos@marcoscarvalho.com>
2018-07-25 10:40:50 -0300

    Ajuste Error Controller

--- src/main/java/hello/ErrorController.java
+++ src/main/java/hello/ErrorController.java
@@ -1,90 +1,67 @@
 package hello;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-	/**
-	 * Error Attributes in the Application
-	 */
-//	private ErrorAttributes errorAttributes;
+	@Autowired
+	private Logger logger;
 
 	private final static String ERROR_PATH = "/error";
+	private final static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 
-	/**
-     * Controller for the Error Controller
-     * @param errorAttributes
-     */
-//    public ErrorController(ErrorAttributes errorAttributes) {
-//        this.errorAttributes = errorAttributes;
-//    }
-
-	/**
-	 * Supports the HTML Error View
-	 * 
-	 * @param request
-	 * @return
-	 */
-//	@RequestMapping(value = ERROR_PATH, produces = "text/html")
-//	public ModelAndView errorHtml(HttpServletRequest request) {
-//		return new ModelAndView("/errors/error", getErrorAttributes(request, false));
-//	}
-	
 	@RequestMapping(value = ERROR_PATH, produces = "text/html")
-	public String errorHtml(HttpServletRequest request) {
-		return "404";
+	public ModelAndView errorHtml(HttpServletRequest request, Exception e) {
+
+		ModelAndView errorPage = new ModelAndView("404");
+		String errorMsg = "";
+		int httpErrorCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+		String uri = (String) request.getAttribute("javax.servlet.error.request_uri");
+		String data = sdf.format(new Date());
+
+		switch (httpErrorCode) {
+		case 400: {
+			errorMsg = "Http Error Code: 400. Bad Request";
+			break;
+		}
+		case 401: {
+			errorMsg = "Http Error Code: 401. Unauthorized";
+			break;
+		}
+		case 404: {
+			errorMsg = "Http Error Code: 404. Resource not found";
+			break;
+		}
+		case 500: {
+			errorMsg = "Http Error Code: 500. Internal Server Error";
+			break;
+		}
+		}
+		errorPage.addObject("errorMsg", errorMsg);
+		errorPage.addObject("httpErrorCode", httpErrorCode);
+		errorPage.addObject("httpRequestUri", uri);
+		errorPage.addObject("exception", e);
+		errorPage.addObject("data", data);
+
+		String msg = "Error request >> " + httpErrorCode + " >> " + errorMsg + " >> " + uri + " >> " + data + " >> " + e;
+		logger.error(msg);
+
+		return errorPage;
 	}
 
-	/**
-	 * Supports other formats like JSON, XML
-	 * 
-	 * @param request
-	 * @return
-	 */
-//	@RequestMapping(value = ERROR_PATH)
-//	@ResponseBody
-//	public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
-//		Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
-//		HttpStatus status = getStatus(request);
-//		return new ResponseEntity<Map<String, Object>>(body, status);
-//	}
-
-	/**
-	 * Returns the path of the error page.
-	 *
-	 * @return the error path
-	 */
 	@Override
 	public String getErrorPath() {
 		return ERROR_PATH;
 	}
-
-//	private boolean getTraceParameter(HttpServletRequest request) {
-//		String parameter = request.getParameter("trace");
-//		if (parameter == null) {
-//			return false;
-//		}
-//		return !"false".equals(parameter.toLowerCase());
-//	}
-
-//	private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
-//		RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-//		return this.errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
-//	}
-
-//	private HttpStatus getStatus(HttpServletRequest request) {
-//		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-//		if (statusCode != null) {
-//			try {
-//				return HttpStatus.valueOf(statusCode);
-//			} catch (Exception ex) {
-//			}
-//		}
-//		return HttpStatus.INTERNAL_SERVER_ERROR;
-//	}
 
 }
